59a077d7537dbf27b5fa081000ac4a9505ba9771
Kehvarl <Kehvarl@Kehvarl.com>
2025-08-05 18:40:50 -0700

    control the circle

--- app/main.rb
+++ app/main.rb
@@ -1,35 +1,36 @@
 require 'app/lowrez.rb'
 
+def init args
+    args.state.star = {
+        x: 32,
+        y: 32,
+        w: 20,
+        h: 20,
+        anchor_x: 0.5,
+        anchor_y: 0.5,
+        path: 'sprites/circle/yellow.png'
+        }
+
+end
+
 def tick args
-    # How to set the background color
+    if args.tick_count == 0
+        init args
+    end
     args.lowrez.background_color = [64, 64, 64]
 
-    args.lowrez.solids  << { x: 0, y: 64, w: 10, h: 10, r: 255 }
+    if args.inputs.keyboard.space
+        args.state.star.w += 1
+        args.state.star.h += 1
+    end
 
-    args.lowrez.labels  << {
-    x: 32,
-    y: 63,
-    text: "lowrezjam 2025",
-    size_enum: LOWREZ_FONT_SM,
-    alignment_enum: 1,
-    r: 0,
-    g: 0,
-    b: 0,
-    a: 255,
-    font: LOWREZ_FONT_PATH
-    }
+    args.state.star.w -= 0.5
+    args.state.star.h -= 0.5
 
-    args.lowrez.sprites << {
-    x: 32 - 10,
-    y: 32 - 10,
-    w: 20,
-    h: 20,
-    path: 'sprites/lowrez-ship-blue.png',
-    a: Kernel.tick_count % 255,
-    angle: Kernel.tick_count % 360
-    }
+    #a: Kernel.tick_count % 255,
+    #angle: Kernel.tick_count % 360
 
-    args.lowrez.lines << { x: 0, y: 0, x2: 63, y2:  0, r: 255 }
+    args.lowrez.sprites << args.state.star
 end
 
 
